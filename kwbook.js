145df726604af29f0d92aabfde41bb02b420d4c0
kangris-sket <33802726+kangris-sket@users.noreply.github.com>
2018-07-08 14:19:13 +0700

    Update kwbook.js

--- kwbook.js
+++ kwbook.js
@@ -1,9 +1,10 @@
 //Cloak link
 
 //<![CDATA[
-var key = window.location.href.split("go/")[1].replace("/","")
+var key = window.location.href.split("book/")[1].replace("/","")
 var urls={
- 'fb':'https://www.facebook.com/wanovan', //Your Affiliate is http://example.com/go/fb or http://example.blogspot.com/go/fb
+ //Your Affiliate is http://example.com/go/fb or http://example.blogspot.com/go/fb
+ 'book':'https://www.google.com',
  'produk1':"Affiliate link anda",
  'produk2':"Affiliate link anda",
 }
